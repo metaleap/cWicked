8e2396011a50e08d5a24a3cc61023103456a2dd0
Phil Schumann <metaleap@users.noreply.github.com>
2025-01-12 01:48:56 +0100

    fixups

--- src/demo/main.c
+++ src/demo/main.c
@@ -5,7 +5,7 @@
 
 
 int main() {
-  auto sdl_win = wi_sdl2_init_and_create_window("Hello Mondo!!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 960, 600,
+  auto sdl_win = wi_sdl2_init_and_create_window("Hola Mondo!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 960, 600,
                                                 SDL_WINDOW_VULKAN | SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_RESIZABLE);
   sleep(11);
   SDL_DestroyWindow(sdl_win);
